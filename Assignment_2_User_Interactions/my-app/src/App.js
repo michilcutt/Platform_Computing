f72038ba2947a3effb4eb1d9e490cfe648b7e70e
michilcutt <145288129+michilcutt@users.noreply.github.com>
2024-03-06 10:14:51 -0800

    Update App.js
    updating after in-class work

--- Assignment_2_User_Interactions/my-app/src/App.js
+++ Assignment_2_User_Interactions/my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.gif';
 import './App.css';
 
 function App() {
@@ -6,22 +6,36 @@ function App() {
     <div className="App">
             <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-              
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <title>The AFRO CODE</title>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/michilcutt/Platform_Computing.git"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-       Booty Cheeks
+                    rel="noopener noreferrer"
+                    img src="githublink.jpg" alt="cute doggo">
         </a>
       </header>
     </div>
-  );
+    );
+    function First() {
+        return (
+            <div className="First">
+                <p>
+                    <header>
+                        Nonstop grind? Coffee coded? Me too!
+                    </header>
+                    <hr />
+                    Afro Code is like a DNA Sequence from Jurassic Park, always changing and editing
+                    lines of code throughout the different evolutions of themselves. Transferring from a community college
+                    to a university was a step that was new, intriguing but frightening. Few things of comfort came through the
+                    hard journey: coffee(put emoji here), animals(put emoji here), and new surroundings. Along with
+                    these new surroundings, new interests came as well.
+                </p>
+          /**creating a github link button*/
+                <a href="https://github.com/michilcutt/Platform_Computing/tree/main" target="_blank"></a>
+                <button onclick="window.location.href='https://github.com/michilcutt/Platform_Computing.git'">Github</button>
+            </div>
+        );
+    }
 }
 
 export default App;
