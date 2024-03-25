500adca5fecd000359b965545982b839a462cf19
michilcutt <145288129+michilcutt@users.noreply.github.com>
2024-03-25 01:16:33 -0700

    Update metric_trackerwithdatabase.py
    updating code

--- Assignment_3_Database/Metric_tracker/metric_trackerwithdatabase.py
+++ Assignment_3_Database/Metric_tracker/metric_trackerwithdatabase.py
@@ -1,37 +1,78 @@
 import time
+from datetime import datetime, timedelta
+from sqlite3 import Timestamp
 from selenium import webdriver
+import collections
+import csv
 
-# Initialize browser
-driver = webdriver.Chrome()
-
-# Navigate to your website 
-driver.get("http://localhost:3000/")
-
-metrics = []
-# Track presence time 
-start_time = time.time()
-presence_time = start_time
-while True:#presence_time < 50: # seconds
-    current_time = time.time()
-    presence_time = current_time - start_time
-    print(f"Presence time: {presence_time} seconds")
-    
-    # Track scrolling
-    scroll_height = driver.execute_script("return document.body.scrollHeight")  
-    current_scroll = driver.execute_script("return window.pageYOffset")
-    print(f"Scrolled {current_scroll}/{scroll_height} pixels")
-    
-    time.sleep(2) 
-
-    # Track clicks   
-    buttons = driver.find_elements_by_class_name("Button1", "Button2", "Button3")
-    num_clicks = 0
-
-    for button in buttons:
-        button.click()
-        num_clicks += 1
+#Writing to CSV file
+def writeToCSV(filename: str, metrics: dict):
+    with open(file = filename, mode="w", newline="") as fp:
+        writer = csv.DictWriter(fp, fieldnames=metrics[0].keys())
         
-    print(f"Number of clicks: {num_clicks}")
+        #Header Row
+        writer.writeheader()
+        
+        #Data Row
+        for metric in metrics:
+            writer.writerow(metric)
+        
+
+
+def main():
+
+    # Initialize browser
+    driver = webdriver.Chrome()
+
+    # Navigate to your website 
+    driver.get("http://localhost:3000/")
+
+    metrics = [] #collections.defaultdict(list)
+    SAMPLE_SIZE = 3
+    count = 0
+    start_time = time.time()
+    while count < SAMPLE_SIZE:
+    # Track presence time 
+        current_time = time.time()
+        presence_time = current_time - start_time
+        print(f"Presence time: {presence_time} seconds")
+        #metrics["Presence time (Seconds)"].append(presence_time)
     
-while True:
-    print()
+        # Track scrolling
+        scroll_height = driver.execute_script("return document.body.scrollHeight")  
+        current_scroll = driver.execute_script("return window.pageYOffset")
+        print(f"Scrolled: {current_scroll}/{scroll_height} pixels")
+        #metrics["Scrolling: (Pixels)"].append(current_scroll/scroll_height)
+
+        get_title = driver.title
+        print(f"Name: {get_title}")
+        
+        metrics.append({"TIMESTAMP (HH:MM:SS)": time.strftime("%H:%M:%S", time.localtime()),
+                        "Presence Time (Seconds)" : presence_time,
+                        "Scrolling (Pixels)" : current_scroll/scroll_height,
+                        "Name: ": {get_title}
+                        })
+        count += 1
+    
+        time.sleep(5) 
+    
+        #Get Title
+        #get_title = driver.title
+        #print(f"Name: {get_title}")
+        #metrics["Name: "].append(get_title)
+
+        # Track clicks   
+        #python_button = driver.find_elements_by_class_name('Button1''Button2''Button3')
+        #python_button.click()
+        #num_clicks = 0
+        
+        #print(f"Number of clicks: {num_clicks}")
+    
+    #while True:
+     #   print()
+    driver.quit()
+    print(metrics)
+    writeToCSV("metrics.csv", metrics)
+    
+if __name__ == "__main__":
+    main()
